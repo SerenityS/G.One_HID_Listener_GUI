188e9b81df5f70e8ccbd6b45001faa21da8003bc
GV솔그린 <tkdeh339@naver.com>
2021-04-14 21:39:43 +0900

    초기 커밋.

---
+++ G.One_GUI/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace G.One_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Test_btn = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Test_btn
+            // 
+            this.Test_btn.Location = new System.Drawing.Point(703, 408);
+            this.Test_btn.Name = "Test_btn";
+            this.Test_btn.Size = new System.Drawing.Size(85, 30);
+            this.Test_btn.TabIndex = 1;
+            this.Test_btn.Text = "TEST";
+            this.Test_btn.UseVisualStyleBackColor = true;
+            this.Test_btn.Click += new System.EventHandler(this.Test_btn_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox.Size = new System.Drawing.Size(776, 390);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.Test_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Test_btn;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ G.One_GUI/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G.One_GUI
+{
+    using HidLibrary;
+    public partial class Form1 : Form
+    {
+        private static HidDevice _device;
+        private const int VID = 0xFEED;
+        private const int PID = 0x6060;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void AppendText(RichTextBox richTextBox, string text)
+        {
+            if (!richTextBox.InvokeRequired)
+            {
+                text += Environment.NewLine;
+                richTextBox.AppendText(text);
+            }
+            else
+            {
+                richTextBox.Invoke(new Action<RichTextBox, string>(AppendText), richTextBox, text);
+            }
+        }
+        private void Test_btn_Click(object sender, EventArgs e)
+        {
+            AppendText(richTextBox, "TEST 버튼 클릭");
+        }
+        public void Device()
+        {
+            _device = HidDevices.Enumerate(VID, PID).First();
+            if (10 > _device.Capabilities.InputReportByteLength)
+            {
+                _device = HidDevices.Enumerate(VID, PID).Last();
+            }
+
+            if (_device != null)
+            {
+                _device.OpenDevice();
+
+                _device.MonitorDeviceEvents = true;
+
+                _device.Inserted += DeviceAttachedHandler;
+                _device.Removed += DeviceRemovedHandler;
+
+                string testString = "G.One 키보드 인식 완료";
+                AppendText(richTextBox ,testString);
+                Console.WriteLine("G.One 키보드 인식 완료");
+
+                _device.ReadReport(OnReport);
+
+                _device.CloseDevice();
+            }
+            else
+            {
+                MessageBox.Show("키보드 인식 실패!! 키보드 연결 후 확인을 눌러주세요.");
+                Console.WriteLine("키보드 인식 실패");
+                Device();
+            }
+            
+        }
+        private void OnReport(HidReport report)
+        {
+            var data = report.Data;
+            var stringData = string.Empty;
+
+            if (0 < data.Length)
+            {
+                stringData = Encoding.Default.GetString(data);
+            }
+            else
+            {
+                MessageBox.Show("에러!");
+            }
+            Console.WriteLine(stringData);
+            Console.WriteLine(_device.Capabilities.InputReportByteLength);
+            stringData += Environment.NewLine;
+            AppendText(richTextBox, stringData);
+            stringData = string.Empty;
+
+            _device.ReadReport(OnReport);
+        }
+
+        private void DeviceAttachedHandler()
+        {
+            MessageBox.Show("기기가 연결 되었습니다.");
+        }
+
+        private void DeviceRemovedHandler()
+        {
+            MessageBox.Show("기기가 제거되었습니다.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Device();
+        }
+    }
+}
