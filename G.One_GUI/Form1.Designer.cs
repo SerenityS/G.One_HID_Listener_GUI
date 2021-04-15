880cea5f63eac27d969e2893e7a27d1f389b0bd5
GV솔그린 <tkdeh339@naver.com>
2021-04-15 19:28:46 +0900

    MySql.Data 이용, MariaDB 와 연동완료

--- G.One_GUI/Form1.Designer.cs
+++ G.One_GUI/Form1.Designer.cs
@@ -29,48 +29,90 @@ namespace G.One_GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.Test_btn = new System.Windows.Forms.Button();
+            this.DB_bt = new System.Windows.Forms.Button();
             this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.LED_ON_OFF = new System.Windows.Forms.Button();
+            this.MULTITAP_ON_OFF = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // Test_btn
+            // DB_bt
             // 
-            this.Test_btn.Location = new System.Drawing.Point(703, 408);
-            this.Test_btn.Name = "Test_btn";
-            this.Test_btn.Size = new System.Drawing.Size(85, 30);
-            this.Test_btn.TabIndex = 1;
-            this.Test_btn.Text = "TEST";
-            this.Test_btn.UseVisualStyleBackColor = true;
-            this.Test_btn.Click += new System.EventHandler(this.Test_btn_Click);
+            this.DB_bt.Location = new System.Drawing.Point(668, 394);
+            this.DB_bt.Name = "DB_bt";
+            this.DB_bt.Size = new System.Drawing.Size(120, 50);
+            this.DB_bt.TabIndex = 1;
+            this.DB_bt.Text = "DB 로드";
+            this.DB_bt.UseVisualStyleBackColor = true;
+            this.DB_bt.Click += new System.EventHandler(this.DB_btn_Click);
             // 
             // richTextBox
             // 
+            this.richTextBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.richTextBox.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox.ForeColor = System.Drawing.SystemColors.Window;
             this.richTextBox.Location = new System.Drawing.Point(12, 12);
             this.richTextBox.Name = "richTextBox";
             this.richTextBox.ReadOnly = true;
             this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox.Size = new System.Drawing.Size(776, 390);
+            this.richTextBox.Size = new System.Drawing.Size(776, 273);
             this.richTextBox.TabIndex = 2;
             this.richTextBox.Text = "";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 291);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(776, 97);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // LED_ON_OFF
+            // 
+            this.LED_ON_OFF.Location = new System.Drawing.Point(12, 394);
+            this.LED_ON_OFF.Name = "LED_ON_OFF";
+            this.LED_ON_OFF.Size = new System.Drawing.Size(120, 50);
+            this.LED_ON_OFF.TabIndex = 4;
+            this.LED_ON_OFF.Text = "전등 ON/OFF";
+            this.LED_ON_OFF.UseVisualStyleBackColor = true;
+            this.LED_ON_OFF.Click += new System.EventHandler(this.LED_ON_OFF_Click);
+            // 
+            // MULTITAP_ON_OFF
+            // 
+            this.MULTITAP_ON_OFF.Location = new System.Drawing.Point(138, 394);
+            this.MULTITAP_ON_OFF.Name = "MULTITAP_ON_OFF";
+            this.MULTITAP_ON_OFF.Size = new System.Drawing.Size(120, 50);
+            this.MULTITAP_ON_OFF.TabIndex = 5;
+            this.MULTITAP_ON_OFF.Text = "멀티탭 ON/OFF";
+            this.MULTITAP_ON_OFF.UseVisualStyleBackColor = true;
+            this.MULTITAP_ON_OFF.Click += new System.EventHandler(this.MULTITAP_ON_OFF_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MULTITAP_ON_OFF);
+            this.Controls.Add(this.LED_ON_OFF);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.richTextBox);
-            this.Controls.Add(this.Test_btn);
+            this.Controls.Add(this.DB_bt);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Button Test_btn;
+        private System.Windows.Forms.Button DB_bt;
         private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button LED_ON_OFF;
+        private System.Windows.Forms.Button MULTITAP_ON_OFF;
     }
 }
 
--- G.One_GUI/Form1.cs
+++ G.One_GUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace G.One_GUI
 {
@@ -16,10 +17,14 @@ namespace G.One_GUI
         private static HidDevice _device;
         private const int VID = 0xFEED;
         private const int PID = 0x6060;
-
+        private string strConn = "Server=gvsolgryn.nemiku.cc;Database=TestIoT;Uid=iot;Pwd=1q2w3e4r;";
         public Form1()
         {
             InitializeComponent();
+        }
+        static class Underbar
+        {
+            public const string underbar = "-------------------------------------------------";
         }
         public void AppendText(RichTextBox richTextBox, string text)
         {
@@ -27,15 +32,12 @@ namespace G.One_GUI
             {
                 text += Environment.NewLine;
                 richTextBox.AppendText(text);
+                richTextBox.ScrollToCaret();
             }
             else
             {
                 richTextBox.Invoke(new Action<RichTextBox, string>(AppendText), richTextBox, text);
             }
-        }
-        private void Test_btn_Click(object sender, EventArgs e)
-        {
-            AppendText(richTextBox, "TEST 버튼 클릭");
         }
         public void Device()
         {
@@ -68,7 +70,6 @@ namespace G.One_GUI
                 Console.WriteLine("키보드 인식 실패");
                 Device();
             }
-            
         }
         private void OnReport(HidReport report)
         {
@@ -91,20 +92,107 @@ namespace G.One_GUI
 
             _device.ReadReport(OnReport);
         }
-
         private void DeviceAttachedHandler()
         {
-            MessageBox.Show("기기가 연결 되었습니다.");
         }
-
         private void DeviceRemovedHandler()
         {
-            MessageBox.Show("기기가 제거되었습니다.");
+        }
+        public void TableLoad(string sql)
+        {
+            MySqlConnection conn = new MySqlConnection(strConn);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader tableData = cmd.ExecuteReader();
+
+            while (tableData.Read())
+            {
+                Console.WriteLine("ID : {0}\n센서 : {1}\n상태 : {2}\n마지막 사용 시간 : {3}\n" + Underbar.underbar, tableData["ID"], tableData["SENSOR"], tableData["STATUS"], tableData["LAST_Use"]);
+
+                string id, sensor, status, last;
+
+                id = tableData["ID"].ToString();
+                sensor = tableData["SENSOR"].ToString();
+                status = tableData["STATUS"].ToString();
+                last = tableData["LAST_Use"].ToString();
+
+                AppendText(richTextBox, "ID : "+id+'\n'+"센서 이름 : "+sensor+'\n'+"상태 : "+status+'\n' + "마지막 사용 시간" + last + '\n' + Underbar.underbar + '\n');
+            }
+            tableData.Close();
+            conn.Close();
+        }
+
+        public void ChangeStatus(int status, string name)
+        {
+            MySqlConnection conn = new MySqlConnection(strConn);
+            conn.Open();
+            string sql = "SELECT STATUS from test where SENSOR='led'";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            if (status == 0)
+            {
+                //string test = "UPDATE test SET STATUS = 1, LAST_Use = now() WHERE SENSOR = @sensorName";
+                cmd.CommandText = "UPDATE test SET STATUS = 1, LAST_Use = now() WHERE SENSOR = @sensorName";
+                cmd.Parameters.Add("@sensorName", MySqlDbType.VarChar, 20);
+                cmd.Parameters[0].Value = name;
+                cmd.ExecuteNonQuery();
+                AppendText(richTextBox, name + " 켜기 완료!");
+            }
+            else if (status == 1)
+            {
+                cmd.CommandText = "UPDATE test SET STATUS = 0, LAST_Use = now() WHERE SENSOR = @sensorName";
+                cmd.Parameters.Add("@sensorName", MySqlDbType.VarChar, 20);
+                cmd.Parameters[0].Value = name;
+                cmd.ExecuteNonQuery();
+                AppendText(richTextBox, name + " 끄기 완료!");
+            }
+            else AppendText(richTextBox, "쿼리 에러");
+            conn.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             Device();
+        }
+        private void DB_btn_Click(object sender, EventArgs e)
+        {
+            string sqlText = "SELECT * FROM test";
+            TableLoad(sqlText);
+        }
+        private void LED_ON_OFF_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(strConn);
+            conn.Open();
+            string sql = "SELECT STATUS,SENSOR from test where ID='1'";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader tableData = cmd.ExecuteReader();
+            tableData.Read();
+            Console.WriteLine("Status: {0}", tableData["STATUS"]);
+            string name;
+            int status;
+            status = (int)tableData["STATUS"];
+            name = tableData["SENSOR"].ToString();
+            ChangeStatus(status, name);
+            tableData.Close();
+            conn.Close();
+        }
+
+        private void MULTITAP_ON_OFF_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(strConn);
+            conn.Open();
+            string sql = "SELECT STATUS,SENSOR from test where ID='2'";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader tableData = cmd.ExecuteReader();
+            tableData.Read();
+            Console.WriteLine("Status: {0}", tableData["STATUS"]);
+            string name;
+            int status;
+            status = (int)tableData["STATUS"];
+            name = tableData["SENSOR"].ToString();
+            ChangeStatus(status, name);
+            tableData.Close();
+            conn.Close();
         }
     }
 }
